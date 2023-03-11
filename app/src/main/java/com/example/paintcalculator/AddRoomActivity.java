bdbc51ae80eadaa1e8e0935935fdec27888c011b
Mikhail <mike10581@gmail.com>
2023-03-10 22:54:17 -0500

    add saving rooms with number and descriptions add clearing sharedPreferences

--- app/src/main/java/com/example/paintcalculator/AddRoomActivity.java
+++ app/src/main/java/com/example/paintcalculator/AddRoomActivity.java
@@ -2,13 +2,21 @@ package com.example.paintcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.StringWriter;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
 
 public class AddRoomActivity extends AppCompatActivity {
 
@@ -16,30 +24,96 @@ public class AddRoomActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_room);
+
         final SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
 
-        Button button = (Button) findViewById(R.id.button);
-        Button button2 = (Button) findViewById(R.id.button3);
+        Button saveButton = (Button) findViewById(R.id.btnAddRoomSave);
+        Button cancelButton = (Button) findViewById(R.id.btnAddRoomCancel);
+
         SharedPreferences.Editor editor = sharedPref.edit();
-        button.setOnClickListener(new View.OnClickListener() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                EditText widthFT = (EditText) findViewById(R.id.txtRoomWidthFT);
+                EditText widthInch = (EditText) findViewById(R.id.txtRoomWidthInch);
+                EditText lengthFT = (EditText) findViewById(R.id.txtRoomLengthFT);
+                EditText lengthInch = (EditText) findViewById(R.id.txtRoomLengthInch);
+                EditText heightFT = (EditText) findViewById(R.id.txtRoomHeightFT);
+                EditText heightInch = (EditText) findViewById(R.id.txtRoomHeightInch);
+                EditText roomColor = (EditText) findViewById(R.id.txtRoomColor);
 
-                editor.putString("message", "My first message");
-                editor.commit();
+                int width = 0;
+                if (!widthFT.getText().toString().equals("")) {
+                    width += Integer.parseInt(widthFT.getText().toString()) * 12;
+                }
+                if (!widthInch.getText().toString().equals("")) {
+                    width += Integer.parseInt(widthInch.getText().toString());
+                }
 
-                Intent intent = new Intent();
-                setResult(RESULT_OK, intent);
-                finish();
+                int length = 0;
+                if (!lengthFT.getText().toString().equals("")) {
+                    length += Integer.parseInt(lengthFT.getText().toString()) * 12;
+                }
+                if (!lengthInch.getText().toString().equals("")) {
+                    length += Integer.parseInt(lengthInch.getText().toString());
+                }
+
+                int height = 0;
+                if (!heightFT.getText().toString().equals("")) {
+                    height += Integer.parseInt(heightFT.getText().toString()) * 12;
+                }
+                if (!heightInch.getText().toString().equals("")) {
+                    height += Integer.parseInt(heightInch.getText().toString());
+                }
+
+                String color = roomColor.getText().toString();
+                boolean error = color.equals("") || width == 0 || length == 0 || height == 0;
+
+                if (error) {
+                    Toast.makeText(AddRoomActivity.this, "ERROR!!! Missing data.", Toast.LENGTH_SHORT).show();
+                } else {
+                    String res = "Room " + width + "in X " + length + "in X " + height + "in, color: " + color + " was added.";
+
+                    String areaKey = "COLOR:" + roomColor.getText().toString();
+                    String ceilingKey = "CEILING";
+                    String roomsAmountKey = "ROOMS_AMOUNT";
+
+                    int area = sharedPref.getInt(areaKey, 0);
+                    int ceiling = sharedPref.getInt(ceilingKey, 0);
+                    int roomNumber = sharedPref.getInt(roomsAmountKey, 0) + 1;
+
+                    area = area + (width + length) * height * 2;
+                    ceiling = ceiling + width * length;
+
+
+
+
+
+
+
+
+
+
+                    editor.putString("MESSAGE", res);
+                    editor.putInt(areaKey, area);
+                    editor.putInt(ceilingKey, ceiling);
+                    editor.putInt(roomsAmountKey, roomNumber);
+                    String roomKey = "ROOM:" + roomNumber;
+                    String roomDescription = width + ":" + length + ":" + height + ":" + color;
+                    editor.putString(roomKey, roomDescription);
+                    editor.commit();
+
+                    Intent intent = new Intent();
+                    setResult(RESULT_OK, intent);
+                    finish();
+                }
+
             }
         });
 
-        button2.setOnClickListener(new View.OnClickListener() {
+        cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                editor.putString("message", "My second message");
-                editor.commit();
-
                 Intent intent = new Intent();
                 setResult(RESULT_OK, intent);
                 finish();
--- app/src/main/java/com/example/paintcalculator/MainActivity.java
+++ app/src/main/java/com/example/paintcalculator/MainActivity.java
@@ -14,13 +14,22 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     SharedPreferences sharedPref;
-
+    TextView textView;
+//    SharedPreferences.Editor editor = sharedPref.edit();
     // Handle the result of AddRoomActivity
     private ActivityResultLauncher<Intent> addRoomLauncher = registerForActivityResult(
             new ActivityResultContracts.StartActivityForResult(),
@@ -28,10 +37,27 @@ public class MainActivity extends AppCompatActivity {
                 @Override
                 public void onActivityResult(ActivityResult result) {
                     if (result.getResultCode() == RESULT_OK) {
-                        // Run your code here
 
-                        String message = sharedPref.getString("message", "");
-                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+                        String message = sharedPref.getString("MESSAGE", "");
+                        Map<String, ?> dataset = sharedPref.getAll();
+
+                        StringBuilder sb = new StringBuilder();
+                        for (Map.Entry<String, ?> entry : dataset.entrySet()) {
+                            if (!entry.getKey().equals("MESSAGE")) {
+                                sb.append(entry.getKey()).append(": ").append(entry.getValue().toString()).append("\n");
+                            }
+
+                        }
+                        String store = sb.toString();
+
+                        textView.setText(store);
+
+//                        ListView listView = (ListView) findViewById(R.id.lstView);
+//                        listView.setEnt
+//                        entries.addAll(preferences.getAll().entrySet());
+
+
+                        Toast.makeText(MainActivity.this, message, Toast.LENGTH_LONG).show();
 
                     }
                 }
@@ -43,13 +69,44 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button addRoom = (Button) findViewById(R.id.btnAddRoom);
+        Button clear = (Button) findViewById(R.id.btnClear);
+        textView = findViewById(R.id.txtRoomsList);
         sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
         addRoom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, AddRoomActivity.class);
-
                 addRoomLauncher.launch(intent);
+            }
+        });
+
+
+        Map<String, ?> dataset = sharedPref.getAll();
+
+        StringBuilder sb = new StringBuilder();
+        for (Map.Entry<String, ?> entry : dataset.entrySet()) {
+            if (!entry.getKey().equals("MESSAGE")) {
+                sb.append(entry.getKey()).append(": ").append(entry.getValue().toString()).append("\n");
+            }
+
+        }
+        String store = sb.toString();
+
+        textView.setText(store);
+
+
+
+
+        clear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sharedPref.edit();
+                Map<String, ?> dataset = sharedPref.getAll();
+                for (Map.Entry<String, ?> entry : dataset.entrySet()) {
+                    editor.remove(entry.getKey());
+                }
+                editor.commit();
+                textView.setText("");
             }
         });
     }
